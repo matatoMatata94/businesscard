d3383a847b9f2e933956ee637613b8527f9743bf
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2021-04-17 19:25:24 +0200

    Sprinkled some finishing over it

--- lib/showCard.dart
+++ lib/showCard.dart
@@ -17,77 +17,74 @@ class _BusinessCardState extends State<BusinessCard> {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 150,
-                    width: 150,
-                    child: Center(
-                      child: _avatarImage == null
-                          ? Text('No image selected.')
-                          : Image.file(_avatarImage),
-                    ),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.orange,
+              Container(
+                height: 150,
+                width: 150,
+                child: Center(
+                  child: _avatarImage == null
+                      ? Text('No image selected.')
+                      : Image.file(_avatarImage),
+                ),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.orange,
+                ),
+              ),
+              Container(height: 50),
+              Container(
+                child: Text(
+                  'matatoMatata94',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 250,
+                child: Divider(
+                  thickness: 2,
+                  color: Colors.teal[100],
+                ),
+              ),
+              Container(height: 30),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+99 123456789',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.teal[900],
                     ),
                   ),
-                  Container(height: 50),
-                  Container(
-                    child: Text(
-                      'matatoMatata94',
-                      style: TextStyle(
-                        fontFamily: 'Pacifico',
-                        fontSize: 40,
-                      ),
+                ),
+              ),
+              Container(height: 30),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'example@mail.com',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.teal[900],
                     ),
                   ),
-                  Container(height: 30),
-                  Card(
-                    color: Colors.white,
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        '+99 123456789',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.teal[900],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(height: 30),
-                  Card(
-                    color: Colors.white,
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.mail,
-                            color: Colors.teal,
-                          ),
-                          Text(
-                            'example@mail.com',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.teal[900],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ],
           ),
